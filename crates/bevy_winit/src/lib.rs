ae3bf78dcde615e38cc26a554b768f204fc01a86
Aceeri <conmcclusk@gmail.com>
2022-12-10 13:26:09 -0800

    window reader stuff

--- crates/bevy_winit/src/lib.rs
+++ crates/bevy_winit/src/lib.rs
@@ -45,10 +45,8 @@ impl Plugin for WinitPlugin {
         #[cfg(target_arch = "wasm32")]
         app.add_plugin(web_resize::CanvasParentResizePlugin);
         let event_loop = EventLoop::new();
-        #[cfg(not(any(target_os = "android", target_os = "ios", target_os = "macos")))]
+        //#[cfg(not(any(target_os = "android", target_os = "ios", target_os = "macos")))]
         let mut create_window_reader = WinitCreateWindowReader::default();
-        #[cfg(any(target_os = "android", target_os = "ios", target_os = "macos"))]
-        let create_window_reader = WinitCreateWindowReader::default();
         // Note that we create a window here "early" because WASM/WebGL requires the window to exist prior to initializing
         // the renderer.
         // And for ios and macos, we should not create window early, all ui related code should be executed inside
